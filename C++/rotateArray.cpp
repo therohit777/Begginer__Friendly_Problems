519de84e45ae7290968bfdc039b8be12036f817c
SARTHAK RANJAN <56467031+sarthaknerd@users.noreply.github.com>
2022-10-11 10:25:56 +0530

    Create rotateArray.cpp

---
+++ C++/rotateArray.cpp
@@ -0,0 +1,35 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void Rotate(int arr[], int d, int n)
+{
+	int p = 1;
+	while (p <= d) {
+		int last = arr[0];
+		for (int i = 0; i < n - 1; i++) {
+			arr[i] = arr[i + 1];
+		}
+		arr[n - 1] = last;
+		p++;
+	}
+}
+
+void printArray(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << arr[i] << " ";
+}
+
+int main()
+
+{
+	int arr[] = { 1,4,5,7,8};
+	int N = sizeof(arr) / sizeof(arr[0]);
+	int d = 3;
+
+	Rotate(arr, d, N);
+	printArray(arr, N);
+
+	return 0;
+}
