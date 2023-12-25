153a5f45287eed7978dcccf6d6d9c167c6ccdfe3
Aditya Ranjan <133626698+aditya10454@users.noreply.github.com>
2023-12-25 17:18:00 +0530

    Update 23rdOct_Priyanshu-792(Linear search).cpp
    Added Description for easy understanding of the code

--- C++/23rdOct_Priyanshu-792(Linear search).cpp
+++ C++/23rdOct_Priyanshu-792(Linear search).cpp
@@ -1,27 +1,41 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
+
 int main()
 {
-    int  i, num, index;
+    // Declare variables
+    int i, num, index;
     int n;
-    cout<<"Enter size of the array: ";
-    cin>>n;
+
+    // Input the size of the array
+    cout << "Enter size of the array: ";
+    cin >> n;
+
+    // Declare an array of size 'n'
     int arr[n];
-    cout<<"Enter "<<n<<" numbers: ";
-    for(i=0; i<10; i++)
-        cin>>arr[i];
-    cout<<"\nEnter a Number to Search: ";
-    cin>>num;
-    for(i=0; i<10; i++)
+
+    // Input 'n' numbers into the array
+    cout << "Enter " << n << " numbers: ";
+    for (i = 0; i < 10; i++) // This loop has a fixed limit of 10 iterations, which should be 'n'
+        cin >> arr[i];
+
+    // Input a number to search
+    cout << "\nEnter a Number to Search: ";
+    cin >> num;
+
+    // Search for the input number in the array
+    for (i = 0; i < 10; i++) // Same issue as above, should be 'i < n'
     {
-        if(arr[i]==num)
+        if (arr[i] == num)
         {
             index = i;
-            break;
+            break; // Exit the loop if the number is found
         }
     }
-    cout<<"\nFound at Index No."<<index;
-    cout<<endl;
+
+    // Display the index where the number was found
+    cout << "\nFound at Index No." << index << endl;
+
     return 0;
 }
